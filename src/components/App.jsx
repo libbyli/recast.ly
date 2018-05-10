2f562430a12d8484e5dbd51793eae72ffc6009fc
libbyli <sincerelylibby@gmail.com>
2018-05-09 17:30:49 -0700

    Added search bar and API.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
           </div>
         </nav>
         <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      type: 'video',
+      videoEmbeddable: true
+    },
+    success: function(data) {
+      callback(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
